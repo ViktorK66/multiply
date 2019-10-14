f182ef05b609622e6a463cc73967e6cdfca6c7f8
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2019-10-14 15:24:51 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstNumber = first.split('').reverse().map(item => +item);
+  let secondNumber = second.split('').reverse().map(item=> +item);
+  let stack = [];
+  for(let i = 0; i< firstNumber.length; i++){
+    let dozens = 0;
+    for (let f = 0; f< secondNumber.length; f++) {
+      if (stack[i + f]) dozens += stack[i + f];
+      let result = firstNumber[i] * secondNumber[f] + dozens;
+      if (result > 9) {
+        dozens = Math.trunc(result / 10);
+        result = result % 10;
+      } else dozens = 0;
+      stack[i + f] = result;
+    }
+    if (dozens) stack.push(dozens);
+  }
+  return stack.reverse().join('');
 }
